d7f92bad754e46e98b648a1fbeaeeea24012dd2d
leekangmmin <156677806+leekangmmin@users.noreply.github.com>
2026-06-05 08:16:21 +0900

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2025 이강민 (Lee Kangmin) — EJU Score Tracker Service Worker (PWA v2)
-const CACHE_NAME = 'eju-score-v3';
-const STATIC_CACHE = 'eju-static-v3';
+const CACHE_NAME = 'eju-score-v4';
+const STATIC_CACHE = 'eju-static-v4';
 
 self.addEventListener('install', e => {
   self.skipWaiting();
